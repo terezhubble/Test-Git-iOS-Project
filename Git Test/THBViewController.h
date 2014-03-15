a32f5072394629c99c2c2522747dc21725933940
Terez Hubble-Brownfield <terezhubble-brownfield@Terez-Hubble-Brownfields-MacBook-Pro-2.local>
2014-03-15 13:02:57 -0700

    Initial Commit

---
+++ Git Test/THBViewController.h
@@ -0,0 +1,13 @@
+//
+//  THBViewController.h
+//  Git Test
+//
+//  Created by Terez Hubble-Brownfield on 3/15/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface THBViewController : UIViewController
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Terez Hubble-Brownfield on 3/15/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "THBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([THBAppDelegate class]));
+    }
+}
