8d5d65796bfeb6c85a830b02aad67a41554d2cf1
Terez Hubble-Brownfield <terezhubble-brownfield@Terez-Hubble-Brownfields-MacBook-Pro-2.local>
2014-03-15 13:09:55 -0700

    added a test class

---
+++ THBTest.h
@@ -0,0 +1,13 @@
+//
+//  THBTest.h
+//  Git Test
+//
+//  Created by Terez Hubble-Brownfield on 3/15/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface THBTest : NSObject
+
+@end
---
+++ THBTest.m
@@ -0,0 +1,13 @@
+//
+//  THBTest.m
+//  Git Test
+//
+//  Created by Terez Hubble-Brownfield on 3/15/14.
+//
+//
+
+#import "THBTest.h"
+
+@implementation THBTest
+
+@end
